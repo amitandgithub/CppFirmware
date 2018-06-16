19a3a62cadb6c737a9ce86f199b52fa64e5ae3ce
amitandgithub <34563851+amitandgithub@users.noreply.github.com>
2018-06-16 09:35:47 +0530

    changes after long time. But working now.

--- Test/src/Test.cpp
+++ Test/src/Test.cpp
@@ -26,11 +26,11 @@ static UI::ScreenHandle_t myControlScreenHandle;
 uint8_t aCurrent[20];
 uint8_t aCurrent1[20];
 uint8_t aVoltage[9];
-Clock nClock;
-Time aTime(0,0,0,1000);
+//Clock nClock;
+//Time aTime(0,0,0,1000);
 
 static DS3231 rtc(&INA219_I2C,0xD0);
-static HwStopWatch nHwStopWatch(&rtc);
+//static HwStopWatch nHwStopWatch(&rtc);
 
 
 void Init_Tests()
@@ -73,7 +73,7 @@ void RunTests()
     MyUI.EventHamdler(gEvent);
     MyUI.Run();
     INA219_Obj.Run(&Power);
-
+/*
     PowerMonitorScreen.SetText(0, 0, rtc.getDOWStr(DS3231::FORMAT_SHORT), 3);
     PowerMonitorScreen.SetText(0, 3, ",", 1);
     PowerMonitorScreen.SetText(0, 4, rtc.getDateStr(DS3231::FORMAT_LONG, DS3231::FORMAT_LITTLEENDIAN,'-'), 2);
@@ -82,9 +82,10 @@ void RunTests()
     PowerMonitorScreen.SetText(0, 10, "-", 1);
     PowerMonitorScreen.SetText(0, 11, &(rtc.getDateStr(DS3231::FORMAT_LONG, DS3231::FORMAT_LITTLEENDIAN,'-'))[8], 2);
     ftoa(rtc.getTemp(),(char*)aCurrent, 5);
+
     PowerMonitorScreen.SetText(1, 4, (char*)aCurrent , 5);
     PowerMonitorScreen.SetText(2, 4, rtc.getTimeStr(DS3231::FORMAT_LONG), 8);
-
+*/
     ftoa(Power.Voltage, (char*)aVoltage, 6);
     ftoa(Power.Current, (char*)aCurrent, 6);
 
@@ -104,16 +105,16 @@ void RunTests()
     //MilliSecTimer.Run();
     //PowerMonitorScreen.SetText(5, 0, MilliSecTimer.Get((char*)aCurrent), 12);
     //t = rtc.getTime();
-    aTime.Run();
+   // aTime.Run();
     //PowerMonitorScreen.SetText(5, 0,  aTime.Get((char*)aCurrent), 8);
 
     //PowerMonitorScreen.SetText(5, 0,  &(ctime(&mytime))[11], 14);
     //PowerMonitorScreen.SetText(5, 0,  loc_time. , 14);
 
-    nHwStopWatch.Run();
-    PowerMonitorScreen.SetText(5, 0,  nHwStopWatch.GetTimeStr((char*)aCurrent1), 8);
-    nClock.Run();
-    PowerMonitorScreen.SetText(4, 0, nClock.GetCurrentTimeString((char*)aCurrent), 8);
+    //nHwStopWatch.Run();
+    //PowerMonitorScreen.SetText(5, 0,  nHwStopWatch.GetTimeStr((char*)aCurrent1), 8);
+    //nClock.Run();
+   // PowerMonitorScreen.SetText(4, 0, nClock.GetCurrentTimeString((char*)aCurrent), 8);
 
 }
 
@@ -189,9 +190,9 @@ void OneSecondHandler()
 }
 void Create_Alarm_Clock()
 {
-    static Time time(0,0,5);
-    nClockHandle = nClock.CreateAlarm( Clock::ALARM_TYPE_REPEAT, time, OneSecondHandler );
-    nClock.StartAlarm(nClockHandle);
+   // static Time time(0,0,5);
+   // nClockHandle = nClock.CreateAlarm( Clock::ALARM_TYPE_REPEAT, time, OneSecondHandler );
+   // nClock.StartAlarm(nClockHandle);
 }
 
 
